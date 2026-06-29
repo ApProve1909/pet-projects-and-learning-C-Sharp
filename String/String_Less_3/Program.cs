8a2a37a58bfec9f4b42a0638fa34bedb376a4c11
Vladislav Drozd <58953031+ApProve1909@users.noreply.github.com>
2026-06-29 20:33:42 +0300

    Add files via upload

---
+++ String/String_Less_3/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text;
+
+namespace String_less_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello world!!",32);
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine(sb);
+            Console.WriteLine($"Длина: {sb.Length} \nЁмкость: {sb.Capacity}");
+
+            sb.Append("How are your day today?");
+            Console.WriteLine($"Длина: {sb.Length} \nЁмкость: {sb.Capacity}");
+
+            sb.Append("Is there anything that is bothering you in any way?");
+            Console.WriteLine($"Длина: {sb.Length} \nЁмкость: {sb.Capacity}");
+
+            sb.Insert(1, "NO");
+            Console.WriteLine(sb);
+
+            sb.Replace("world!!", "МИР!!");
+            Console.WriteLine(sb);
+
+            string text = sb.ToString();
+            Console.WriteLine(text);
+        }
+    }
+}
